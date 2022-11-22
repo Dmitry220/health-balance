385e509bb364ed93641817bc0c383d45c4a4458c
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2022-11-22 12:50:53 +0300

    input chat

--- src/pages/Chat-page/Personal-chat.tsx
+++ src/pages/Chat-page/Personal-chat.tsx
@@ -1,16 +1,13 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import paperclip from '../../assets/image/chat/icon_paperclip.svg'
+import Header from '../../Components/Header/Header'
 import './chat-page.scss'
 
 export const PersonalChat = () => {
 
 	const id = Number(localStorage.getItem("id"))
-
 	const [message, setMessage] = useState('')
-
 	const [messages, setMessages] = useState<any>([])
-
-	const [connected, setConnected] = useState<boolean>(false)
-
 	const socket:any = useRef()
 
 	const sendMessage = () => {
@@ -41,17 +38,14 @@ export const PersonalChat = () => {
 			// }		
 			// socket.current.send(JSON.stringify(message))
 		}
-
 		socket.current.onmessage= (event:any) => {				
 			const message = JSON.parse(event.data)
 			setMessages((prev:any)=>[...prev,message])
 			console.log("message");			
 		}
-
 		socket.current.onclose= () => {
 			console.log("close");			
 		}
-
 		socket.current.onerror= () => {
 			console.log("error");			
 		}
@@ -60,46 +54,38 @@ export const PersonalChat = () => {
 		// console.log(a);
 		
 	}, [])
-	
+
+
 	
   return (
 	 <div className='personal-chat'>
-		<div className="personal-chat__messages">
-		{/* <div className="personal-chat__message message-item">
-			<div className='message-item_name text-blue'>Хелпер</div>
-			<div className="message-item__text">
-				Добрый день! Усейн, как я могу вам помочь?
+		{/* <Header title='Чат' /> */}
+		<div className="personal-chat__container">
+			<div className="personal-chat__messages">
+				{
+					messages.map((mess:any, i:any)=>(
+						<div className="personal-chat__message message-item" style={{marginLeft: id === mess.id ? 'auto' : 0}} key={i} id={'qw'+i}>
+							<div className="message-item__text">
+								{mess?.message}
+							</div>
+							<div className="message-item__time">{new Date().getHours() + ':' + new Date().getMinutes()}</div>
+				</div>
+					))
+				}
+			</div>		
+			<div className="personal-chat__send">
+				<button className='personal-chat__paperclip'><img src={paperclip} alt="paperclip" /></button>
+				<input 
+				type="text" 
+				className="personal-chat__input" 
+				placeholder='Сообщение' 
+				onChange={(e:ChangeEvent<HTMLInputElement>)=>setMessage(e.target.value)}
+				value={message}
+				/>
+				<button className="personal-chat__submit icon-icon_send" onClick={sendMessage}></button>
 			</div>
-			<div className="message-item__time">12:32</div>
 		</div>
-		<div className="personal-chat__message message-item" style={{marginLeft: 'auto'}}>
-			<div className="message-item__text">
-				Ничем!
-			</div>
-			<div className="message-item__time">12:32</div>
-		</div> */}
-		{
-			messages.map((mess:any, i:any)=>(
-				<div className="personal-chat__message message-item" style={{marginLeft: id === mess.id ? 'auto' : 0}} key={i} id={'qw'+i}>
-					<div className="message-item__text">
-						{mess?.message}
-					</div>
-					<div className="message-item__time">12:32</div>
-		</div>
-			))
-		}
-		</div>
-		
-		<div className="personal-chat__send">
-			<input 
-			type="text" 
-			className="personal-chat__input" 
-			placeholder='Сообщение' 
-			onChange={(e:ChangeEvent<HTMLInputElement>)=>setMessage(e.target.value)}
-			value={message}
-			/>
-			<button className="personal-chat__submit icon-icon_send" onClick={sendMessage}></button>
-		</div>
+		<div className="line"></div>
 	 </div>
   )
 }
