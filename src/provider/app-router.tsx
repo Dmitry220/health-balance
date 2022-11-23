5df846df283ca8ab00071731afcf7e29e1c6f7f3
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2022-11-23 16:48:32 +0300

    fix auth

--- src/provider/app-router.tsx
+++ src/provider/app-router.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import {privateRoutes, publicRoutes} from "./routes";
-import {Routes, Route,} from "react-router-dom";
-import {AuthPage} from "../pages/Authorization/Auth-page";
-import {ActivityPage} from "../pages/Activity-page/Activity-page";
+import { privateRoutes, publicRoutes } from "./routes";
+import { Routes, Route, } from "react-router-dom";
+import { AuthPage } from "../pages/Authorization/Auth-page";
+import { ActivityPage } from "../pages/Activity-page/Activity-page";
 import { useAppDispatch, useAppSelector } from '../utils/hooks/redux-hooks';
 import { checkAuth, isAuthSelector, isLoadingSelector } from '../Redux/slice/authSlice';
 import { setUserData } from '../Redux/slice/profileSlice';
@@ -14,42 +14,45 @@ const AppRouter = () => {
     const user = localStorage.getItem('token')
     const isAuth = useAppSelector(isAuthSelector)
     const isLoading = useAppSelector(isLoadingSelector)
-    const dispatch= useAppDispatch()
+    const dispatch = useAppDispatch()
 
-    const idUser = Number(localStorage.getItem("id"))   
+    const idUser = Number(localStorage.getItem("id"))
 
-    useEffect(()=>{
-       if(idUser && !isLoading){
-            dispatch(checkAuth())                     
-            dispatch(setUserData(idUser))   
+    useEffect(() => {   
+        console.log('router');
+            
+        if (isAuth && !isLoading) {
+            dispatch(setUserData(idUser))
+        }else{
+            dispatch(checkAuth())
         }
-           
-    }, [])
 
-    if(isLoading){
+    }, [isAuth])
+
+    if (isLoading) {
         return <h1>Загрузка...</h1>
     }
 
-console.log('app');
+    console.log('app');
 
 
-   
+
 
     return isAuth && user ?
         (
             <Routes>
-                {privateRoutes.map(({path, Component}, index) =>
-                    <Route path={path} key={index} element={<Component />}/>
+                {privateRoutes.map(({ path, Component }, index) =>
+                    <Route path={path} key={index} element={<Component />} />
                 )}
-                <Route path={'*'}  element={<ActivityPage />}/>
+                <Route path={'*'} element={<ActivityPage />} />
             </Routes>
         ) :
         (
             <Routes>
-                {publicRoutes.map(({path, Component}, index) =>
-                    <Route path={path} key={index} element={<Component />}/>
+                {publicRoutes.map(({ path, Component }, index) =>
+                    <Route path={path} key={index} element={<Component />} />
                 )}
-                <Route path={'*'} element={<AuthPage />}/>
+                <Route path={'*'} element={<AuthPage />} />
             </Routes>
         )
 };
