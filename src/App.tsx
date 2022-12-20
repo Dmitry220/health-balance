0886390c8eda10de4994eacd0aca4f04fb1ac35d
Victor <victor_byte@icloud.com>
2022-12-20 19:01:06 +0300

    push notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react'
+import OneSignal from 'onesignal-cordova-plugin'
 import './assets/style/global.scss'
 import AppRouter from './provider/app-router'
 
 function App() {
+  useEffect(() => {
+    OneSignalInit()
+  }, [])
+
+  function OneSignalInit(): void {
+    OneSignal.setAppId('6c585b11-b33a-44f5-8c7b-3ffac2059d19')
+    OneSignal.setNotificationOpenedHandler(function (jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData))
+    })
+    OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
+      console.log('User accepted notifications: ' + accepted)
+    })
+  }
+
   return (
     <div className={'_container'}>
       <AppRouter />
