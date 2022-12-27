ff07978b4299c77f6a6149aac74835cec5d43b05
Victor <victor_byte@icloud.com>
2022-12-27 14:06:58 +0300

    https fix

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const API_URL = 'http://test-1.symedia.ru/api/'
+export const API_URL = 'https://test-1.symedia.ru/api/'
 
 export const $api = axios.create({ baseURL: API_URL })
 
-export const IMAGE_URL = 'http://test-1.symedia.ru/assets/images/'
+export const IMAGE_URL = 'https://test-1.symedia.ru/assets/images/'
 
-export const DOC_URL = 'http://test-1.symedia.ru/downloads/'
+export const DOC_URL = 'https://test-1.symedia.ru/downloads/'
