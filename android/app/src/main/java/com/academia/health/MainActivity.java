b12cf21c5b44f48e57928e438b332073ea55448c
Victor Batischev <vitek.batishev97@gmail.com>
2023-01-20 11:13:18 +0300

    remove plugin

--- android/app/src/main/java/com/academia/health/MainActivity.java
+++ android/app/src/main/java/com/academia/health/MainActivity.java
@@ -2,14 +2,12 @@ package com.academia.health;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.adscientiam.capacitor.googlefit.GoogleFitPlugin;
 
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(PedometerPlugin.class);
-        registerPlugin(GoogleFitPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
