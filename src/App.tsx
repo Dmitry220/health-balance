85035b61e2b3fd1c418cfb474a682cc1cacade71
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-01-31 18:17:19 +0300

    add user id in oneSignal

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,7 @@
-import { useEffect } from 'react'
-import { Capacitor } from '@capacitor/core'
-import OneSignal from 'onesignal-cordova-plugin'
-
 import './assets/style/global.scss'
 import AppRouter from './provider/app-router'
-import { Device } from '@capacitor/device'
 
 function App() {
-  useEffect(() => {
-    OneSignalInit()
-  }, [])
-
-  async function OneSignalInit () {
-    if (Capacitor.getPlatform() !== 'web') {
-      const uuid = await Device.getId()   
-      let externalUserId = uuid.uuid      
-      console.log('uuid device token ', externalUserId);
-      OneSignal.setAppId('6c585b11-b33a-44f5-8c7b-3ffac2059d19')
-      OneSignal.setNotificationOpenedHandler(function (jsonData) {
-        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData))
-      })
-      OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
-        console.log('User accepted notifications: ' + accepted)
-      })
-
-      OneSignal.setExternalUserId(externalUserId, (results:any) => {
-        // The results will contain push and email success statuses
-        console.log('Results of setting external user id ', JSON.stringify(results));
-               
-        // Push can be expected in almost every situation with a success status, but
-        // as a pre-caution its good to verify it exists
-        if (results.push && results.push.success) {
-          console.log('Results of setting external user id push status: ',results.push.success);       
-        }
-        
-        // Verify the email is set or check that the results have an email success status
-        if (results.email && results.email.success) {
-          console.log('Results of setting external user id email status: ',results.email.success);        
-        }
-      
-        // Verify the number is set or check that the results have an sms success status
-        if (results.sms && results.sms.success) {
-          console.log('Results of setting external user id sms status:');
-          console.log(results.sms.success);
-        }
-      });
-     
-    }
-  }
 
   return (
     <div className={'_container'}>
--- src/Components/Authorization/Auth.tsx
+++ src/Components/Authorization/Auth.tsx
@@ -10,6 +10,9 @@ import {
 import { useAppDispatch } from '../../utils/hooks/redux-hooks'
 import { sendLogin } from '../../Redux/slice/authSlice'
 import { Device } from '@capacitor/device'
+import { Capacitor } from '@capacitor/core'
+import OneSignal from 'onesignal-cordova-plugin'
+
 
 export const Auth = () => {
   const [email, setEmail] = useState<string>('')
@@ -31,7 +34,46 @@ export const Auth = () => {
     const device_token = uuid.uuid
 
     await dispatch(sendLogin({ email, password, device_token}))
+    OneSignalInit()    
     navigate(ACTIVITY_ROUTE)
+  }
+
+  async function OneSignalInit () {    
+    if (Capacitor.getPlatform() !== 'web') {  
+      let externalUserId = localStorage.getItem("id")     
+
+      console.log('uuid device token ', externalUserId);
+      OneSignal.setAppId('6c585b11-b33a-44f5-8c7b-3ffac2059d19')
+      OneSignal.setNotificationOpenedHandler(function (jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData))
+      })
+      OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
+        console.log('User accepted notifications: ' + accepted)
+      })
+
+      OneSignal.setExternalUserId(externalUserId, (results:any) => {
+        // The results will contain push and email success statuses
+        console.log('Results of setting external user id ', JSON.stringify(results));
+               
+        // Push can be expected in almost every situation with a success status, but
+        // as a pre-caution its good to verify it exists
+        if (results.push && results.push.success) {
+          console.log('Results of setting external user id push status: ',results.push.success);       
+        }
+        
+        // Verify the email is set or check that the results have an email success status
+        if (results.email && results.email.success) {
+          console.log('Results of setting external user id email status: ',results.email.success);        
+        }
+      
+        // Verify the number is set or check that the results have an sms success status
+        if (results.sms && results.sms.success) {
+          console.log('Results of setting external user id sms status:');
+          console.log(results.sms.success);
+        }
+      });
+     
+    }
   }
 
   return (
