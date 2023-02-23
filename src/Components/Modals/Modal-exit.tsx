d246fd97f1af3383579bac9cb4fc20b6297873f9
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-02-23 20:23:03 +0300

    fix modal exit

--- src/Components/Modals/Modal-exit.tsx
+++ src/Components/Modals/Modal-exit.tsx
@@ -7,13 +7,14 @@ interface IModalExit {
   text?: string
 }
 
-export const ModalExit: FC<IModalExit> = ({ closeCallback, actionCallback,text }) => {
+export const ModalExit: FC<IModalExit> = ({ closeCallback, actionCallback, text }) => {
   return (
     <div className="modal">
       <div className="modal__background"></div>
       <div className="modal__wrapper">
         <div className="modal__button-wrapper">
-          <div className="modal__button modal__button_exit" onClick={() => actionCallback()}>{text||'Вы действительно хотите выйти из аккаунта?'}</div>
+          <div className="modal__text">Вы действительно хотите выйти из аккаунта?</div>
+          <div className="modal__button modal__button_exit" onClick={() => actionCallback()}>{text || 'Выйти'}</div>
           <div className="modal__button" onClick={() => closeCallback(false)}>Отмена</div>
         </div>
       </div>
