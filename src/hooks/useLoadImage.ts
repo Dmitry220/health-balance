7f48524e3f03719c29003af71f1792e4a61bb706
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-04-11 14:08:20 +0300

    fix image

--- src/hooks/useLoadImage.ts
+++ src/hooks/useLoadImage.ts
@@ -1,58 +1,52 @@
-import { Camera, CameraResultType } from '@capacitor/camera'
-import { useState } from 'react'
-import FileService from '../services/FilesServices'
-import { showToast } from '../utils/common-functions'
-import { typeImage } from '../utils/enums'
+import { Camera, CameraResultType } from "@capacitor/camera";
+import { useState } from "react";
+import FileService from "../services/FilesServices";
+import { showToast } from "../utils/common-functions";
+import { typeImage } from "../utils/enums";
+import axios from "axios";
 
 export const useLoadImage = () => {
-  const [image, setImage] = useState<any>('')
-  const [photoPath, setPhotoPath] = useState<any | null>(null)
-  const [isLoadingAvatar, setIsLoadingAvatar] = useState<boolean>(false)
+  const [image, setImage] = useState<any>("");
+  const [photoPath, setPhotoPath] = useState<any | null>(null);
+  const [isLoadingAvatar, setIsLoadingAvatar] = useState<boolean>(false);
 
   const clearImages = () => {
-    setIsLoadingAvatar(false)
-    setPhotoPath('')
-    setImage('')
-  }
+    setIsLoadingAvatar(false);
+    setPhotoPath("");
+    setImage("");
+  };
 
   const uploadImage = async (type: typeImage) => {
-    let image 
     try {
-      image = await Camera.getPhoto({
+      const image = await Camera.getPhoto({
         quality: 50,
         allowEditing: true,
         resultType: CameraResultType.Uri,
-        promptLabelPhoto: 'Выбрать фото из галерии',
-        promptLabelPicture: 'Сделать фотографию',
-        promptLabelHeader: 'Фото',
-        promptLabelCancel: 'Отмена'
-      })
-    } catch (error) {
-      console.log(error)
-      return
-    }
-    setIsLoadingAvatar(true)
-    try {
-      let imageUrl = image.webPath || ''
-      let blob = await fetch(imageUrl).then((r) => r.blob())
+        promptLabelPhoto: "Выбрать фото из галерии",
+        promptLabelPicture: "Сделать фотографию",
+        promptLabelHeader: "Фото",
+        promptLabelCancel: "Отмена",
+      });
+      setIsLoadingAvatar(true);
+      let imageUrl = image.webPath || "";
+      let blob = await fetch(imageUrl).then((r) => r.blob());
       if (blob) {
-        const response = await FileService.uploadImage(blob, type)
+        const response = await FileService.uploadImage(blob, type);
         if (response?.data?.data?.avatar) {
-          setPhotoPath(imageUrl)
-          setImage(response.data.data.avatar)
-        } else {
-          await showToast('Максимальный вес изображения 3 мб')
+          setPhotoPath(imageUrl);
+          setImage(response.data.data.avatar);
         }
-      } else {
-        await showToast('Изображения нет')
       }
     } catch (error) {
-      clearImages()
-      await showToast('Максимальный вес изображения 3 мб')
+      console.log(error);
+      if (axios.isAxiosError(error)) {
+        await showToast("Максимальный вес изображения 3 мб");
+      }
+      clearImages();
     } finally {
-      setIsLoadingAvatar(false)
+      setIsLoadingAvatar(false);
     }
-  }
+  };
 
-  return [image, photoPath, isLoadingAvatar, clearImages, uploadImage] as const
-}
+  return [image, photoPath, isLoadingAvatar, clearImages, uploadImage] as const;
+};
