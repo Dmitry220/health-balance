cc0c645fc81bdd0127c0d4c263165f52e0690b1d
Victor <victor_byte@icloud.com>
2023-04-11 15:40:36 +0300

    fix error message

--- src/hooks/useLoadImage.ts
+++ src/hooks/useLoadImage.ts
@@ -1,20 +1,20 @@
-import { Camera, CameraResultType } from "@capacitor/camera";
-import { useState } from "react";
-import FileService from "../services/FilesServices";
-import { showToast } from "../utils/common-functions";
-import { typeImage } from "../utils/enums";
-import axios from "axios";
+import { Camera, CameraResultType } from '@capacitor/camera'
+import { useState } from 'react'
+import FileService from '../services/FilesServices'
+import { showToast } from '../utils/common-functions'
+import { typeImage } from '../utils/enums'
+import axios from 'axios'
 
 export const useLoadImage = () => {
-  const [image, setImage] = useState<any>("");
-  const [photoPath, setPhotoPath] = useState<any | null>(null);
-  const [isLoadingAvatar, setIsLoadingAvatar] = useState<boolean>(false);
+  const [image, setImage] = useState<any>('')
+  const [photoPath, setPhotoPath] = useState<any | null>(null)
+  const [isLoadingAvatar, setIsLoadingAvatar] = useState<boolean>(false)
 
   const clearImages = () => {
-    setIsLoadingAvatar(false);
-    setPhotoPath("");
-    setImage("");
-  };
+    setIsLoadingAvatar(false)
+    setPhotoPath('')
+    setImage('')
+  }
 
   const uploadImage = async (type: typeImage) => {
     try {
@@ -22,31 +22,33 @@ export const useLoadImage = () => {
         quality: 50,
         allowEditing: true,
         resultType: CameraResultType.Uri,
-        promptLabelPhoto: "Выбрать фото из галерии",
-        promptLabelPicture: "Сделать фотографию",
-        promptLabelHeader: "Фото",
-        promptLabelCancel: "Отмена",
-      });
-      setIsLoadingAvatar(true);
-      let imageUrl = image.webPath || "";
-      let blob = await fetch(imageUrl).then((r) => r.blob());
+        promptLabelPhoto: 'Выбрать фото из галерии',
+        promptLabelPicture: 'Сделать фотографию',
+        promptLabelHeader: 'Фото',
+        promptLabelCancel: 'Отмена'
+      })
+      setIsLoadingAvatar(true)
+      let imageUrl = image.webPath || ''
+      let blob = await fetch(imageUrl).then((r) => r.blob())
       if (blob) {
-        const response = await FileService.uploadImage(blob, type);
+        const response = await FileService.uploadImage(blob, type)
         if (response?.data?.data?.avatar) {
-          setPhotoPath(imageUrl);
-          setImage(response.data.data.avatar);
+          setPhotoPath(imageUrl)
+          setImage(response.data.data.avatar)
         }
       }
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
       if (axios.isAxiosError(error)) {
-        await showToast("Максимальный вес изображения 3 мб");
+        await showToast('Произошла ошибка при загрузке изображения')
       }
-      clearImages();
+      if (error.errorMessage === 'Error loading image') {
+        await showToast('Максимальный вес изображения  3 мб')
+      }
+      clearImages()
     } finally {
-      setIsLoadingAvatar(false);
+      setIsLoadingAvatar(false)
     }
-  };
+  }
 
-  return [image, photoPath, isLoadingAvatar, clearImages, uploadImage] as const;
-};
+  return [image, photoPath, isLoadingAvatar, clearImages, uploadImage] as const
+}
