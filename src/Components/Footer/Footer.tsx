05748d89ed869d79402fd431c71ad1aaf0370229
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-06-07 22:32:26 +0300

    fix footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,7 +4,10 @@ import './Footer.scss'
 export const Footer = () => {
 	return (
 		<div className='footer'>
-			<a href="mailto:info@health-balance.ru"><span>Техподдержка: </span>info@health-balance.ru</a>
+			<div className="footer__body">
+				<a href="mailto:info@health-balance.ru"><span>Техподдержка: </span>info@health-balance.ru</a>
+				<a href="https://health-balance.ru/web/instruction"><span>Инструкция к приложению: </span>https://health-balance.ru/web/instruction</a>
+			</div>
 		</div>
 	)
 }
